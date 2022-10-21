26fba7b2d388198b038037cc0707cc0519e53a47
juananl292 <juananl292@gmail.com>
2022-10-21 13:29:01 +0200

    nuevas funciones

--- prZoologicoDAO/src/prZoologicoDAO/AnimalDAO.java
+++ prZoologicoDAO/src/prZoologicoDAO/AnimalDAO.java
@@ -2,16 +2,65 @@ package prZoologicoDAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public abstract class AnimalDAO {
 	private static Connection connection;
+	//Buscar animal por id
 	
+	public static Animal findById (int id) {
+		
+		connection = openConnection();
+		String query="select * from animales where id = ?";
+		Statement statement;
+		Animal animal=null;
+		
+		
+		try {
+			PreparedStatement ps=connection.prepareStatement(query);
+			ps.setInt(1,id);
+			ResultSet rs=ps.executeQuery();
+			
+		while(rs.next()) {
+			 animal= new Animal(
+					rs.getInt("id"),
+					rs.getString("nombre"),
+					rs.getString("habitat"),
+					rs.getDouble("peso_aproximado"));
+		}
+			
+			
+			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		closeConnection();
+		
+		return animal;
+	}
+	
+	//Borrar un animal segun nombre
+	public static void deleteAnimalByNombre(String nombre) {
+		connection = openConnection();
+		String query="delete from animales where nombre = '" + nombre + "'";
+		Statement statement;
+		
+		try {
+			statement = connection.createStatement();
+			statement.executeUpdate(query);
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		closeConnection();
+	}
 	
 	
 	//Borrar todos los animales
-	public static void deleteAnimal() {
+	public static void deleteAllAnimal() {
 		connection = openConnection();
 		
 		String query="delete from animales";
@@ -20,10 +69,10 @@ public abstract class AnimalDAO {
 			statement = connection.createStatement();
 			statement.executeUpdate(query);
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		}
-		
+		closeConnection();
 	}
 	
 	
--- prZoologicoDAO/src/prZoologicoDAO/Main.java
+++ prZoologicoDAO/src/prZoologicoDAO/Main.java
@@ -5,8 +5,13 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Animal a= new Animal ("Ardilla", "Bosque", 0.10);
+		Animal b= new Animal ("mono", "Selva", 1.10);
 		AnimalDAO.insertarAnimal(a);
-		AnimalDAO.deleteAnimal();
+		AnimalDAO.insertarAnimal(b);
+		//AnimalDAO.deleteAllAnimal();
+		AnimalDAO.deleteAnimalByNombre("mono");
+		
+		System.out.println(AnimalDAO.findById(7));
 	}
 
 }
